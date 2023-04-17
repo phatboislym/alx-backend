2800cf133591d73112ba057fb6238ab72b8259d0
Adewole Conde <phatboislym@protonmail.com>
2023-04-17 23:41:45 +0100

    add: task 3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """
+        Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """
+        args:   self
+                index: int = None
+                page_size: int = 10
+        return: hyper_index_dict: Dict
+        """
+        dataset = self.indexed_dataset()
+
+        try:
+            assert index >= 0 and index < len(dataset)
+        except AssertionError:
+            raise
+
+        data: List = []
+        next_index = index
+
+        for i in range(index, len(dataset)):
+            if len(data) == page_size:
+                break
+            if i in dataset.keys():
+                data.append(dataset[i])
+                next_index = i + 1
+
+        hyper_index_dict: Dict = {
+            'index': index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index
+        }
+
+        return (hyper_index_dict)
