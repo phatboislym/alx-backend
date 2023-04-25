844ae4c9e5d239b7550779b25dcbd9247e834ef6
Adewole Conde <phatboislym@protonmail.com>
2023-04-26 00:51:20 +0100

    add: task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+"""
+module contains:
+    `app`: basic flask app
+    `Config`: class for flask configuration
+    `get_locale`: function matches text with supported languages
+"""
+
+from flask import Flask, render_template as render, request
+from flask_babel import Babel, gettext
+from typing import Union
+
+app: Flask = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class `Config` for Flask app configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_TRANSLATION_DIRECTORIES = 'translations'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """
+    get_locale method
+    args:   None
+    return: str | None
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """
+    route which renders an index.html template
+    args:   None
+    return: `home_title`: str (as title (<title>))
+            `home_header`: str (as header (<h1>))
+    """
+    title = gettext("home_title")
+    header = gettext("home_header")
+    return render('3-index.html', title=title, header=header)
+
+
+if __name__ == '__main__':
+    """
+    starts the Flask application server
+    debug set to True for hot/live reloading
+    """
+    app.run(debug=True)
