5b29bcc56dbebe75916a6748bc30d99a1e21c1d3
Adewole Conde <phatboislym@protonmail.com>
2023-04-25 21:25:07 +0100

    add: task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+module for a basic Flask app
+contains a single / route
+"""
+
+from flask import Flask, render_template as render, request
+from flask_babel import Babel, gettext
+
+
+app: Flask = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class `Config` for Flask app configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """
+    route which renders an index.html template
+    args:   None
+    return: `Hello world`: str (as header (<h1>))
+    """
+    return render('2-index.html')
+
+
+if __name__ == '__main__':
+    """
+    starts the Flask application server
+    debug set to True for hot/live reloading
+    """
+    app.run(debug=True)
