87c46b2ee54364b36a8a0a2675332ec82d84869c
Adewole Conde <phatboislym@protonmail.com>
2023-05-22 23:50:22 +0100

    add: task 4

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,46 @@
+// redis client connects to a Redis server running on the default port
+// it logs a message to the console to indicate the connection status
+
+import redis from 'redis';
+import { promisify } from 'util';
+
+const redisClient = redis.createClient();
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+
+/* createHash:
+* it accepts three arguments: key, field and value
+* it should set in Redis the key, field and value
+* it should display a confirmation message using redis.print
+*/
+function createHash(key, field, value) {
+  redisClient.hset(key, field, value, redis.print);
+}
+
+/* displayHash:
+* it accepts one argument key
+* it should log to the console the hash for the key passed as argument
+*/
+function displayHash(key) {
+  redisClient.hgetall(key, (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(result);
+    }
+  });
+}
+
+createHash('HolbertonSchools', 'Portland', '50');
+createHash('HolbertonSchools', 'Seattle', '80');
+createHash('HolbertonSchools', 'New York', '20');
+createHash('HolbertonSchools', 'Bogota', '20');
+createHash('HolbertonSchools', 'Cali', '40');
+createHash('HolbertonSchools', 'Paris', '2');
+displayHash('HolbertonSchools');
